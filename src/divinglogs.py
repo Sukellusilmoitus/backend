28bf09d93190876564691e8ce2bfb12a2e8bed52
Shmuli02 <samuli.kaleva@gmail.com>
2022-01-25 12:12:49 +0000

    add divinglogs.py

---
+++ src/divinglogs.py
@@ -0,0 +1,5 @@
+def create_new_diving_log(diver_id, wreck_id, location_is_correct,
+                            x_coordinate, y_coordinate, new_coordinate_text,
+                            changes_on_wreck, changes_info, created_at):
+    
+    return
