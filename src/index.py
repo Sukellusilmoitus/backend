7624bbe8af90b6caf0e683405c3fada0577894e7
Shmuli02 <samuli.kaleva@gmail.com>
2022-01-25 15:50:54 +0000

    update app routes to api routes

--- src/index.py
+++ src/index.py
@@ -41,9 +41,6 @@ class NewDivingLog(Resource):
         divinglogs.create_new_diving_log(diver_id, wreck_id, location_is_correct,
                                             x_coordinate, y_coordinate, new_coordinate_text,
                                             changes_on_wreck, changes_info, created_at)
-        print(diver_id, wreck_id, location_is_correct,
-                                            x_coordinate, y_coordinate, new_coordinate_text,
-                                            changes_on_wreck, changes_info, created_at)
         return {'message': 'Data received'}
 
 @api.route("/api/wrecks")
