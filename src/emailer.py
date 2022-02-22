1bf4e68b0e865b006cfad7ef8c3041c0c0e58c49
Shmuli02 <samuli.kaleva@gmail.com>
2022-02-22 13:37:03 +0000

    improve code quality

--- src/emailer.py
+++ src/emailer.py
@@ -1,5 +1,6 @@
 import datetime
-import smtplib, ssl
+import smtplib
+import ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from util.config import SENDER_EMAIL, SENDER_EMAIL_PASSWORD, RECEIVER_EMAIL, SERVER_URL
@@ -18,11 +19,12 @@ class Emailer:
         date = datetime.datetime.now() - datetime.timedelta(days=self.days)
         date = date.replace(hour=23, minute=59, second=59)
         self.dives = self.dive_model.objects.raw({'created_at': {'$gte': date}})
-    
+
     def get_targets(self):
         date = datetime.datetime.now() - datetime.timedelta(days=self.days)
         date = date.replace(hour=23, minute=59, second=59)
-        self.targets = self.target_model.objects.raw({'source': 'ilmoitus', 'created_at': {'$gte': date}})
+        self.targets = self.target_model.objects.raw(
+            {'source': 'ilmoitus', 'created_at': {'$gte': date}})
 
     def send_email(self):
         self.get_dives()
@@ -30,15 +32,14 @@ class Emailer:
         sender_email = SENDER_EMAIL
         receiver_email = RECEIVER_EMAIL
         password = SENDER_EMAIL_PASSWORD
-        
 
-        message = MIMEMultipart("alternative")
-        message["Subject"] = "Sukellusilmoituksia"
-        message["From"] = sender_email
-        message["To"] = receiver_email
+        message = MIMEMultipart('alternative')
+        message['Subject'] = 'Sukellusilmoituksia'
+        message['From'] = sender_email
+        message['To'] = receiver_email
 
-        length_target = len([i for i in self.targets])
-        length_dives = len([i for i in self.dives])
+        length_target = len(list(self.targets))
+        length_dives = len(list(self.dives))
 
         html = f"""
         <html>
@@ -66,7 +67,7 @@ class Emailer:
                         <th>ID</th>
                         <th>Sukeltaja</th>
                     </tr>"""
-                
+
         for dive in self.dives:
             id = dive.to_json()['id']
             html += f"""
@@ -79,15 +80,12 @@ class Emailer:
                 </table>
             </body>
         </html>"""
-        
-        part = MIMEText(html, "html")
-        message.attach(part)
-        aa =  message.as_string()
-        print(aa)
 
-        send = input('Lähetä')
+        part = MIMEText(html, 'html')
+        message.attach(part)
+
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
             server.login(sender_email, password)
             server.sendmail(
                 sender_email, receiver_email, message.as_string()
