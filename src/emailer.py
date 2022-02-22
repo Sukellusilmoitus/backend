c9c7abcf8e7f82fee963fc6c3e29d35bc8d3c9a2
Shmuli02 <samuli.kaleva@gmail.com>
2022-02-22 13:28:26 +0000

    update emailer content as HTML

--- src/emailer.py
+++ src/emailer.py
@@ -1,8 +1,8 @@
 import datetime
-from mimetypes import MimeTypes
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from util.config import SENDER_EMAIL, SENDER_EMAIL_PASSWORD, RECEIVER_EMAIL, SERVER_URL
 
 class Emailer:
     def __init__(self,dive_model, user_model, util, target_model, days):
@@ -24,3 +24,71 @@ class Emailer:
         date = date.replace(hour=23, minute=59, second=59)
         self.targets = self.target_model.objects.raw({'source': 'ilmoitus', 'created_at': {'$gte': date}})
 
+    def send_email(self):
+        self.get_dives()
+        self.get_targets()
+        sender_email = SENDER_EMAIL
+        receiver_email = RECEIVER_EMAIL
+        password = SENDER_EMAIL_PASSWORD
+        
+
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Sukellusilmoituksia"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+
+        length_target = len([i for i in self.targets])
+        length_dives = len([i for i in self.dives])
+
+        html = f"""
+        <html>
+            <body>
+                <p>Uudet hylyt ({length_target})</p>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nimi</th>
+                    </tr>"""
+
+        for target in self.targets:
+            id = target.to_json()['properties']['id']
+            html += f"""
+                    <tr>
+                        <th><a href='{SERVER_URL}/hylyt/{id}'>{id}</a></th>
+                        <th>{target.name}</th>
+                    </tr>"""
+
+        html += f"""
+                </table>
+                <p>Uudet sukellusilmoitukset ({length_dives})</p>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Sukeltaja</th>
+                    </tr>"""
+                
+        for dive in self.dives:
+            id = dive.to_json()['id']
+            html += f"""
+                    <tr>
+                        <th>{id}</th>
+                        <th>{dive.diver.name}</th>
+                    </tr>"""
+
+        html += """
+                </table>
+            </body>
+        </html>"""
+        
+        part = MIMEText(html, "html")
+        message.attach(part)
+        aa =  message.as_string()
+        print(aa)
+
+        send = input('Lähetä')
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(
+                sender_email, receiver_email, message.as_string()
+            )
