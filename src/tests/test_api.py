e0923a1134664b2d53a33feb366aeaeabb4e4dfd
Skorp7 <sanna.korpi@live.fi>
2022-03-22 15:05:19 +0200

    overlapping targets by 4decimal precision + pytests

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,4 +1,4 @@
-# pylint: disable=unused-import
+# pylint: disable=unused-import,line-too-long
 import unittest
 import datetime
 import pytest
@@ -15,12 +15,20 @@ BASE_URL = 'http://localhost:5000/api'
 @pytest.mark.api
 class TestApiEndpoints(unittest.TestCase):
     def setUp(self):
+        print('setupRUNN')
         users = User.objects.all()
         dives = Dive.objects.all()
+        targets = Target.objects.raw({
+            '$or':
+                [{'source': {'$eq': 'ilmoitus'}},
+                {'name': {'$eq': 'overlapHylky'}}]
+        }).all()
         for user in users:
             user.delete()
         for dive in dives:
             dive.delete()
+        for target in targets:
+            target.delete()
 
     def test_api_is_up(self):
         response = requests.get(f'{BASE_URL}/healthcheck').json()
@@ -68,7 +76,73 @@ class TestApiEndpoints(unittest.TestCase):
         user = User.create(name='test user', email='test@example.com', phone='1234567')
         targetnote = Targetnote(diver=user, target=target)
         targetnote.save()
-        response = requests.get(f'{BASE_URL}/targets/pending').json()
-        first_feature = response['features'][0]
-        self.assertGreater(len(response['features']), 0)
-        self.assertTrue(first_feature['target']['properties']['is_pending'])
+        response = requests.get(f'{BASE_URL}/admin/pending?_end=10&_order=ASC&_sort=id&_start=0').json()
+        first_feature = response[0]
+        self.assertGreater(len(response), 0)
+        self.assertTrue(first_feature['is_pending'])
+
+    def test_admin_endpoint_returns_overlapping_targets(self):
+        self.setUp()
+        Target.create(target_id='99999992',
+                      name='overlapHylky',
+                      town='SaimaaTesti',
+                      type='Hylky',
+                      x_coordinate=25.223335,
+                      y_coordinate=61.545545,
+                      location_method='gpstesti',
+                      location_accuracy='huonotesti',
+                      url='https://testiurl.com',
+                      created_at=datetime.datetime.now(),
+                      is_ancient=False,
+                      source='museovirasto',
+                      is_pending=False)
+        Target.create(target_id='9999991',
+                      name='overlapHylky',
+                      town='SaimaaTesti',
+                      type='Hylky',
+                      x_coordinate=25.223335,
+                      y_coordinate=61.545545,
+                      location_method='gpstesti',
+                      location_accuracy='huonotesti',
+                      url='https://testiurl.com',
+                      created_at=datetime.datetime.now(),
+                      is_ancient=False,
+                      source='ilmoitus',
+                      is_pending=True)
+        response = requests.get(f'{BASE_URL}/admin/duplicates?_end=10&_order=ASC&_sort=id&_start=0').json()
+        self.assertGreater(len(response), 0)
+        self.assertIn(response[0]['id'], ['9999991', '99999992'])
+        self.assertIn(response[1]['id'], ['9999991', '99999992'])
+
+    def test_admin_endpoint_returns_overlapping_targets_by_4dec_precicion(self):
+        self.setUp()
+        Target.create(target_id='99999994',
+                      name='overlapHylky',
+                      town='SaimaaTesti',
+                      type='Hylky',
+                      x_coordinate=25.223335,
+                      y_coordinate=61.545545,
+                      location_method='gpstesti',
+                      location_accuracy='huonotesti',
+                      url='https://testiurl.com',
+                      created_at=datetime.datetime.now(),
+                      is_ancient=False,
+                      source='museovirasto',
+                      is_pending=False)
+        Target.create(target_id='9999995',
+                      name='overlapHylky',
+                      town='SaimaaTesti',
+                      type='Hylky',
+                      x_coordinate=25.223322,
+                      y_coordinate=61.5455,
+                      location_method='gpstesti',
+                      location_accuracy='huonotesti',
+                      url='https://testiurl.com',
+                      created_at=datetime.datetime.now(),
+                      is_ancient=False,
+                      source='ilmoitus',
+                      is_pending=False)
+        response = requests.get(f'{BASE_URL}/admin/duplicates?_end=10&_order=ASC&_sort=id&_start=0').json()
+        self.assertGreater(len(response), 0)
+        self.assertIn(response[0]['id'], ['9999995', '99999994'])
+        self.assertIn(response[1]['id'], ['9999995', '99999994'])
