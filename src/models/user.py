95e57b103d9fee9a987b15ee925423e043419db4
Shmuli02 <72216231+Shmuli02@users.noreply.github.com>
2022-03-30 10:48:25 +0300

    Update user.py
    User modelin create funktiossa username ja password oletus arvoksi None

--- src/models/user.py
+++ src/models/user.py
@@ -15,7 +15,7 @@ class User(MongoModel):
         final = True
 
     @staticmethod
-    def create(name, email, phone, username, password):
+    def create(name, email, phone, username=None, password=None):
         user = User(name, email, phone, username, password)
         user.save()
         return user
